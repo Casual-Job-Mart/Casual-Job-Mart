366a41307fc67cc8fa2a813258da6b0c6118985c
rwu780 <rwu780@gmail.com>
2018-04-18 23:01:40 -0600

    initial commit

---
+++ app/src/androidTest/java/com/company/casual_job_mart/casualJobMart/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.company.casual_job_mart.casualJobMart
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.company.casual_job_mart.casual_job_mart", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/company/casual_job_mart/casualJobMart/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.company.casual_job_mart.casualJobMart
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
