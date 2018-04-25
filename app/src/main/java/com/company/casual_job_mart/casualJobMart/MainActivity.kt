6b5f45752df95dad6af468b3ad7e718eada07ee1
restart365 <restart365@gmail.com>
2018-04-24 23:00:47 -0600

    create User class

--- app/src/main/java/com/company/casual_job_mart/casualJobMart/MainActivity.kt
+++ app/src/main/java/com/company/casual_job_mart/casualJobMart/MainActivity.kt
@@ -2,12 +2,18 @@ package com.company.casual_job_mart.casualJobMart
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        creatUser()
+    }
+    fun creatUser(){
+        var user1 = User(1)
+        user1.setPhone("7808503263")
+        Toast.makeText(this,user1.getPhone(),Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/company/casual_job_mart/casualJobMart/User.kt
@@ -0,0 +1,46 @@
+package com.company.casual_job_mart.casualJobMart
+
+class User constructor(idNumber: Int){
+    private var id: Int = idNumber
+    private var phone: String? = null
+    private var email: String? = null
+    private var lastName: String? = null
+    private var firstName: String? = null
+
+    public fun getID():Int {
+        return this.id
+    }
+
+    public fun setPhone(phoneNumber:String){
+        this.phone = phoneNumber
+
+    }
+
+    public fun getPhone():String? {
+        return this.phone
+    }
+
+    public fun setEmail(emailAddr:String){
+        this.email = emailAddr
+    }
+
+    public fun getEmail():String?{
+        return this.email
+    }
+
+    public fun setLastName(lastName:String){
+        this.lastName = lastName
+    }
+
+    public fun getLastName():String?{
+        return this.lastName
+    }
+
+    public fun setFirstName(firstName:String){
+        this.firstName = firstName
+    }
+
+    public fun getFirstName():String?{
+        return this.firstName
+    }
+}
