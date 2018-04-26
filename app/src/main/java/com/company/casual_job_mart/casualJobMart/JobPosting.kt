c5da1f85b0172b7bb3ce53e12155f917e918abf7
rwu780 <rwu780@gmail.com>
2018-04-25 21:40:36 -0600

    Start on Job posting Activity. Add check permission function

--- app/src/main/java/com/company/casual_job_mart/casualJobMart/JobPosting.kt
+++ app/src/main/java/com/company/casual_job_mart/casualJobMart/JobPosting.kt
@@ -2,11 +2,34 @@ package com.company.casual_job_mart.casualJobMart
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
 
 class JobPosting : AppCompatActivity() {
+
+    var MY_REQUEST_CAMERA: Int = 8000
+    var MY_REQUEST_READ_STORAGE: Int = 8001
+    var MY_REQUEST_WRITE_STORAGE: Int = 8002
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_job_posting)
+    }
+
+    fun checkPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), MY_REQUEST_CAMERA)
+        }
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), MY_REQUEST_READ_STORAGE)
+        }
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), MY_REQUEST_WRITE_STORAGE)
+        }
     }
 }
