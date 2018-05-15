ae54efa23ecc192c9e58ebb7b40ff9baab743604
unknown <zli7@ualberta.ca>
2018-05-14 23:40:01 -0600

    start working on search job page

--- app/src/main/java/com/company/casual_job_mart/casualJobMart/SearchJobFragment.kt
+++ app/src/main/java/com/company/casual_job_mart/casualJobMart/SearchJobFragment.kt
@@ -5,13 +5,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 
 class SearchJobFragment : Fragment() {
 
     lateinit var myView: View
+    lateinit var searchView:SearchView
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         myView = inflater!!.inflate(R.layout.fragment_search_job,container,false)
+        searchView = myView.findViewById(R.id.searchViewSearchJob)
+
         return myView
     }
 }
