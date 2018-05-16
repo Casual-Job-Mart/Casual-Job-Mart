4d364b394cee4f07d0e673564a9ac5288678c8cd
unknown <zli7@ualberta.ca>
2018-05-16 15:16:55 -0600

    quick save

---
+++ app/src/main/java/com/company/casual_job_mart/casualJobMart/SearchFragment.kt
@@ -0,0 +1,17 @@
+package com.company.casual_job_mart.casualJobMart
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SearchFragment : Fragment() {
+
+    lateinit var myView: View
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        myView = inflater!!.inflate(R.layout.fragment_message,container,false)
+        return myView
+    }
+}
--- app/src/main/java/com/company/casual_job_mart/casualJobMart/SearchJobFragment.kt
+++ app/src/main/java/com/company/casual_job_mart/casualJobMart/SearchJobFragment.kt
@@ -5,17 +5,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.SearchView
 
 class SearchJobFragment : Fragment() {
 
     lateinit var myView: View
     lateinit var searchView:SearchView
+    lateinit var imageSearch:ImageView
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         myView = inflater!!.inflate(R.layout.fragment_search_job,container,false)
-        searchView = myView.findViewById(R.id.searchViewSearchJob)
-
+        imageSearch = myView.findViewById(R.id.imageViewSearch)
+        imageSearch.setOnClickListener{
+            fragmentManager.beginTransaction().replace(R.id.content_frame,SearchFragment()).commit()
+        }
         return myView
     }
 }
