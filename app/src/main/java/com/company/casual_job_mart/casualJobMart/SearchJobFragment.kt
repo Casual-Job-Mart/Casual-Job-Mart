e131e98c0f91f602b3538d11801a42494b3ad033
unknown <zli7@ualberta.ca>
2018-05-18 11:43:35 -0600

    ready to merge

--- app/src/main/java/com/company/casual_job_mart/casualJobMart/SearchJobFragment.kt
+++ app/src/main/java/com/company/casual_job_mart/casualJobMart/SearchJobFragment.kt
@@ -6,16 +6,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ListView
 import android.widget.SearchView
+import com.google.firebase.database.*
 
 class SearchJobFragment : Fragment() {
 
     lateinit var myView: View
+    lateinit var ref: DatabaseReference
+    lateinit var jobList:MutableList<Job>
+    lateinit var listView: ListView
 
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         myView = inflater!!.inflate(R.layout.fragment_search_job,container,false)
 
+        ref = FirebaseDatabase.getInstance().getReference("jobs")
+        jobList = mutableListOf()
+        listView = myView.findViewById(R.id.listViewJobList)
+
+        loadData()
+
         return myView
+    }
+
+    fun loadData(){
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(p0: DataSnapshot?) {
+                if(p0!!.exists()){
+                    jobList.clear()
+                    for(j in p0.children){
+                        val job = j.getValue(Job::class.java)
+                        jobList.add(job!!)
+                    }
+                    val adapter = JobAdapter(activity,R.layout.jobs,jobList)
+                    listView.adapter = adapter
+                }
+            }
+
+        });
     }
 }
